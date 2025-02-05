3b472d087457ca83fca07df70c41ae63e3b4fe0e
Artem Kriukov <art.kriukov@gmail.com>
2025-02-05 13:49:10 +0300

    add project settings

---
+++ Quizzler/App/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  Quizzler
+//
+//  Created by Artem Kriukov on 05.02.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+
+  
+}
+
---
+++ Quizzler/Helpers/Extension+UIElements.swift
@@ -0,0 +1,9 @@
+//
+//  Extension+UIElements.swift
+//  Quizzler
+//
+//  Created by Artem Kriukov on 05.02.2025.
+//
+
+import Foundation
+
